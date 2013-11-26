52084786b6085aa01fa9edf125d1deb66729a6ba
Darkos.cpp <darkos.cpp@gmail.com>
2013-11-26 04:31:48 +0300

    Add and fix CSS styles for phones, tablets,desktop PC

--- front-end/app/js/app.js
+++ front-end/app/js/app.js
@@ -7,12 +7,15 @@ angular.module('restApp', ['restApp.filters', 'restApp.services', 'restApp.direc
     $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
     $routeProvider.when('/news/:id', {templateUrl: 'partials/news.html', controller: 'MainCtrl'});
     $routeProvider.when('/gallery', {templateUrl: 'partials/gallery.html', controller: 'MainCtrl'});
+    $routeProvider.when('/gallery/:alias', {templateUrl: 'partials/gallery.item.html', controller: 'MainCtrl'});
     $routeProvider.when('/contacts', {templateUrl: 'partials/contacts.html', controller: 'MainCtrl'});
     $routeProvider.when('/history', {templateUrl: 'partials/history.html', controller: 'MainCtrl'});
     $routeProvider.when('/vacancies', {templateUrl: 'partials/vacancies.html', controller: 'MainCtrl'});
     $routeProvider.when('/ballroom', {templateUrl: 'partials/ballroom.html', controller: 'MainCtrl'});
     $routeProvider.when('/restaurant', {templateUrl: 'partials/restaurant.html', controller: 'MainCtrl'});
     $routeProvider.when('/pizzeria', {templateUrl: 'partials/pizzeria.html', controller: 'MainCtrl'});
+    $routeProvider.when('/recover', {templateUrl: 'partials/recover.html', controller: 'MainCtrl'});
+    $routeProvider.when('/registration', {templateUrl: 'partials/registration.html', controller: 'MainCtrl'});
 
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
--- front-end/app/js/objects.js
+++ front-end/app/js/objects.js
@@ -11,28 +11,25 @@ var mainJs = {
   		$('.mobile-menu').html($('.main-menu').html());
   		if (! $('.mobile-menu').hasClass('visible')){
   			$('.mobile-menu').show();
-			$('.mobile-menu').addClass('rounded');
-			$('.mobile-menu').addClass('visible');
-	  		$('.mobile-menu').animate({
-	  			left: "0px",
-	  		},500);
+  			$('.mobile-menu').addClass('rounded');
+  			$('.mobile-menu').addClass('visible');
+	  		$('.mobile-menu').animate({left: "0px"},500);
 
-  			$('.btn-menu').animate({
-	  			left: "273px",
-	  		},500);
+  			$('.btn-menu').animate({left: "273px"},500);
 
   		}else{
 	  		$('.mobile-menu').animate({
-	  			left: "-255px",
-	  		},
-	  		{
-    			duration: 500,
-				complete: function() {
-				    $('.mobile-menu').removeClass('rounded');
-  					$('.mobile-menu').removeClass('visible');
-  					$('.mobile-menu').hide();
-				}
-    		});
+  	  			left: "-255px",
+  	  		},
+  	  		{
+      			duration: 500,
+  				complete: function() {
+  				    $('.mobile-menu').removeClass('rounded');
+    					$('.mobile-menu').removeClass('visible');
+    					$('.mobile-menu').hide();
+  				}
+      	});
+
     		$('.btn-menu').animate({
 	  			left: "0px",
 	  		},500);
@@ -81,5 +78,9 @@ var mainJs = {
       var descriptonElement = $(element).parent().parent().children('.position-description');
       $(descriptonElement).toggle("slow");
 
+    },
+    changePanoranView: function(className, url){
+      var $iframe = $('.' + className);
+      $iframe.attr('src', url);
     }
 }
